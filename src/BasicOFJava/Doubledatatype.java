5f0edc12bdadb12ec0195ab462dc44bc0b024ace
Sanjeet-Sahu <samsahu42@gmail.com>
2025-11-28 13:08:47 +0530

    java practice program

---
+++ src/BasicOFJava/Doubledatatype.java
@@ -0,0 +1,6 @@
+package BasicOFJava;
+public class Doubledatatype {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/BasicOFJava/Incredcrement.java
@@ -0,0 +1,6 @@
+package BasicOFJava;
+public class Incredcrement {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
